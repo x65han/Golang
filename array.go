57d983a9cdb7c2f43b887f4d8c3b29315298adc4
Xiao Han <xiahan@blackberry.com>
2016-11-08 16:33:14 -0500

    function.go

--- array.go
+++ array.go
@@ -2,23 +2,18 @@ package main
 import "fmt"
 func main(){
 	// Create Array
-	var arr [5]int
-	arr[0] = 1997
-	arr[1] = 1998
-	arr[2] = 1999
-	arr[3] = 2000
-	arr[4] = 2001
-	fmt.Println(arr); 
+	arr := []int{1997,1998,1999,2000,2001}
+	fmt.Println(arr);
 	// Create Slice Array
 	sliceArr := make([]int,3)
-	fmt.Println(sliceArr); 
+	fmt.Println(sliceArr);
 	// Append Array
 	appendArr := append(sliceArr, 2016, 2017);
-	fmt.Println(appendArr); 
+	fmt.Println(appendArr);
 	// Copy Array
 	appendArr[0] = 2013
 	appendArr[1] = 2014
 	appendArr[2] = 2015
-	copy(sliceArr, appendArr);
-	fmt.Println(sliceArr);
+	copy(arr, appendArr);
+	fmt.Println(arr);
 }
--- function.go
+++ function.go
@@ -2,5 +2,17 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("<<<<<<<<<<>>>>>>>>>>");
+		list := []int{1,2,3,4,5,6,7,8,8}
+		fmt.Println(list);
+		fmt.Println(average(list));
+}
+func average(input []int) float64{
+		total := 0.0
+		for _,x := range input{
+				total += float64(x)
+		}
+		return round(total/float64(len(input)) )
+}
+func round(input float64) float64{
+		return float64(int(input * 100)) / 100.0
 }
